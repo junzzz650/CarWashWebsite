a5d8593b401c58bda2d8f07b01f730743595dc51
junzzz650 <73089865+junzzz650@users.noreply.github.com>
2024-04-04 17:42:01 +0800

    script modify

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const closeButton = document.querySelector('.close-nav')
 const openButton = document.querySelector('.open-nav');
 const nav = document.querySelector('.nav')
+const navLink = document.querySelectorAll('.nav-link')
 
 
 closeButton.addEventListener ("click", () => {
@@ -9,4 +10,10 @@ closeButton.addEventListener ("click", () => {
 
 openButton.addEventListener("click", () => {
     nav.classList.add('navigation-open');
-}); 
+})
+
+navLink.forEach(function(element) {
+    element.addEventListener("click", () => {
+        nav.classList.remove('navigation-open')
+    } )
+});
